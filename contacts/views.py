0bedf0741fc636e0bca7ee6d3ede443ccb3d8475
Michu <mnadrajkowski@wp.pl>
2025-05-20 20:38:43 +0200

    Pierwszy commit

---
+++ contacts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    contacts = request.user.contacts.all().order_by('-created_at')
+    context = {'contacts':contacts}
+    return render(request, 'contacts.html', context)
