a1594d68860dd71fe9228e6dc0f07df8b6c31fd2
Michu <mnadrajkowski@wp.pl>
2025-05-23 22:16:23 +0200

    Pierwszy commit

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
@@ -6,3 +7,14 @@ def index(request):
     contacts = request.user.contacts.all().order_by('-created_at')
     context = {'contacts':contacts}
     return render(request, 'contacts.html', context)
+
+
+@login_required
+def search_contacts(request):
+    query = request.GET.get('search', '')
+
+    # use the query to filter contacts by name or email
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    )
+    return render(request, 'partials/contact-list.html',{'contacts' : contacts})
