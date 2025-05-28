dc02265db35b10cef5456db3a9ec63e57ced5894
Michu <mnadrajkowski@wp.pl>
2025-05-28 20:54:29 +0200

    Dodano modal

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,24 +1,20 @@
 from django import forms
 from .models import Contact
 
-# class ContactForm(forms.ModelForm):
-#
-#     name = forms.CharField(
-#         widget=forms.TextInput(attrs={
-#             'class':'input-bordered w-full',
-#             'placeholder':'Contact Name'
-#         })
-#     )
-#
-#    email = forms.CharField(
-#         widget=forms.TextInput(attrs={
-#             'class':'input-bordered w-full',
-#             'placeholder':'Contact Name'
-#         })
-#
-#     )
-#     class Meta:
-#         model = Contact
-#         fields = (
-#             'name', 'email'
-#         )
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class':'input-bordered w-full',
+            'placeholder':'Contact Name'}))
+
+    email = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class':'input-bordered w-full',
+            'placeholder':'Contact Name'
+        }))
+
+    class Meta:
+        model = Contact
+        fields = (
+            'name', 'email'
+        )
--- contacts/views.py
+++ contacts/views.py
@@ -2,15 +2,23 @@ from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import ContactForm
+
 @login_required
 def index(request):
     contacts = request.user.contacts.all().order_by('-created_at')
-    context = {'contacts':contacts}
+    context = {
+        'contacts':contacts,
+        'form' : ContactForm()
+
+    }
     return render(request, 'contacts.html', context)
 
 
 @login_required
 def search_contacts(request):
+    import time
+    time.sleep(2)
 
     query = request.GET.get('search', '')
 
